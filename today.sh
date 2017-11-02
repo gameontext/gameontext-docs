8ce199e4ef307894b7ea316e5d8885f767f10e3f
Erin Schnabel <schnabel@us.ibm.com>
2017-11-02 15:39:56 -0700

    remove relative path

--- today.sh
+++ today.sh
@@ -28,10 +28,10 @@ if [ -n "$ENCRYPTION_LABEL" ]; then
   ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
   ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
   ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-  openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../deploy_rsa.enc -out ../deploy_rsa -d
-  chmod 600 ../deploy_rsa
+  openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_rsa.enc -out deploy_rsa -d
+  chmod 600 deploy_rsa
   eval `ssh-agent -s`
-  ssh-add ../deploy_rsa
+  ssh-add deploy_rsa
 fi
 
 # Now that we're all set up, we can push.
